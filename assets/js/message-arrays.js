070b9f26174b4b3631637f2445addff99b39d5a8
Isaque Santana <104996528+isaquesantana@users.noreply.github.com>
2024-06-27 20:52:08 -0300

    Upload Project

---
+++ assets/js/message-arrays.js
@@ -0,0 +1,11 @@
+let messageResult = [
+    'Precisa estudar mais! <img class="iconMessage" src="assets/images/icon-alert.svg">',
+    'Você pode fazer melhor! <img class= "iconMessage" src = "assets/images/icon-exclamation.svg">',
+    'Mandou bem! <img class="iconMessage" src="assets/images/icon-crown.svg">'
+];
+
+let responseStatus = [
+    '<div class="responseStatus-correct"> <img src="assets/images/icon-correct.svg" alt="icon-correct" /> Resposta certa!</div > ',
+    '<div class="responseStatus-incorrect"> <img src="assets/images/icon-incorrect.svg" alt="icon-correct" /> Resposta errada!</div>'
+]
+
---
+++ assets/js/questions.js
@@ -0,0 +1,82 @@
+let questions = [
+    {
+        question: "O que é uma escala cromática?",
+        options: [
+            "Uma sequência de notas que se sucedem por intervalos de tons inteiros.",
+            "Uma sequência de notas que se sucedem por intervalos de terças maiores.",
+            "Uma sequência de 13 notas que se sucedem por intervalos de semitons cromáticos e semitons diatônicos.",
+            "Uma sequência de 13 notas que se sucedem por intervalos de tons e semitons."
+        ],
+        answer: 2
+    },
+    {
+        question: "O que é uma escala diatônica?",
+        options: [
+            "Uma sequência de 8 notas que se sucedem por intervalos de tons e semitons diatônicos.",
+            "Uma sequência de 8 notas que se sucedem por intervalos de semitons cromáticos.",
+            "Uma sequência de notas que se sucedem por intervalos de terças menores.",
+            "Uma sequência de 12 notas que se sucedem por intervalos de tons inteiros."
+        ],
+        answer: 0
+    },
+    {
+        question: "O que é escala musical?",
+        options: [
+            "Uma sequência de notas que se sucedem por intervalos de quartas.",
+            "Um conjunto de notas organizadas aleatoriamente.",
+            "Notas tocadas simultaneamente para formar acordes.",
+            "Notas ordenadas de acordo com suas alturas."
+        ],
+        answer: 3
+    },
+    {
+        question: 'O som tem quatro propriedades, quais são elas?',
+        options: [
+            'Altura, volume, timbre e eco',
+            'Frequência, amplitude, timbre e eco',
+            'Altura, intensidade, timbre e duração',
+            'Frequência, intensidade, timbre e reverberação'
+        ],
+        answer: 2
+    },
+    {
+        question: 'A música divide-se em três partes, quais são elas?',
+        options: [
+            'Melodia, harmonia e ritmo',
+            'Compasso, melodia e harmonia',
+            'Compasso, ritmo e acorde',
+            'Ritmo, acorde e melodia'
+        ],
+        answer: 0
+    },
+    {
+        question: 'O que é melodia?',
+        options: [
+            'Sons que se ouvem simultaneamente',
+            'Sons que se ouvem ou se emitem sucessivamente, ou seja; Um após o outro',
+            'Sons que se emitem em diferentes volumes',
+            'Sons que variam em timbre e intensidade'
+        ],
+        answer: 1
+    },
+    {
+        question: 'O que é harmonia?',
+        options: [
+            'Sons que se ouvem ou se emitem sucessivamente, ou seja, um após o outro',
+            'Sons que variam em intensidade e volume',
+            'Sons que se ouvem ou se emitem simultaneamente, ou seja, ao mesmo tempo',
+            'Sons que se destacam pela sua melodia'
+        ],
+        answer: 2
+    },
+    {
+        question: 'O que é ritmo?',
+        options: [
+            'É a combinação de sons simultâneos que formam acordes e harmonias',
+            'É a organização de sons em uma sequência melódica',
+            'É a variação de timbres e intensidades em uma composição musical',
+            'É o elemento que movimenta os sons, dando-lhes uma cadência agradável, ou seja, é a divisão dos compassos'
+        ],
+        answer: 3
+    }
+]
